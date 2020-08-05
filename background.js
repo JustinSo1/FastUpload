1406173eb24d2cae06b53e6342e550a3b9a4719a
JustinSo1 <41768142+JustinSo1@users.noreply.github.com>
2020-08-05 01:00:38 -0400

    Justin/3 (#4)
    * feat(background): GET request test
    Add new GET request for jsonplaceholder
    
    * feat(background) Add new jquery dependency
    
    Project now uses jQuery
    
    * feat(background): OAuth authorization event
    Users must now login with their gmail account to utilize the extension.
    Google Extension IDs are now synced due to key field in manifest
    
    Closes #3

--- background.js
+++ background.js
@@ -1,11 +1,16 @@
 const setUpContextMenus = () => {
   chrome.contextMenus.create({
     id: "Upload",
-    title: "GUpload",
+    title: "EzUpload",
     type: "normal",
     contexts: ["image"],
   });
 };
+chrome.contextMenus.onClicked.addListener(() => {
+  chrome.identity.getAuthToken({ interactive: true }, function (token) {
+    console.log(token);
+  });
+});
 chrome.runtime.onInstalled.addListener(() => {
   // When the app gets installed, set up the context menus
   setUpContextMenus();
