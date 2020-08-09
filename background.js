8ceb982eb01456da7af162c78bb35773fcae84a4
JustinSo1 <sojustin28@gmail.com>
2020-08-09 01:45:51 -0400

    style(background): change scoping of variables Change variable scopes Removal of debug statements

--- background.js
+++ background.js
@@ -8,53 +8,42 @@ const setUpContextMenus = () => {
 };
 
 function dataURItoBlob(dataURI) {
-  var binary = window.atob(dataURI.split(",")[1]);
-  var array = [];
-  for (var i = 0; i < binary.length; i++) {
+  const binary = window.atob(dataURI.split(",")[1]);
+  let array = [];
+  for (let i = 0; i < binary.length; ++i) {
     array.push(binary.charCodeAt(i));
   }
   return new Blob([new Uint8Array(array)], { type: "image/jpeg" });
 }
 
 chrome.contextMenus.onClicked.addListener((info) => {
-  console.log(info);
-
-  srcUrl = info.srcUrl;
-  mimeType = srcUrl.substring(srcUrl.indexOf(":") + 1, srcUrl.indexOf(";"));
-
-  // const BASE64_MARKER = ";base64,";
-  // const base64Index = srcUrl.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
-  // const base64 = srcUrl.substring(base64Index);
-
-  // console.log(base64);
-  // const binaryData = window.atob(base64);
-  // console.log(binaryData);
-  binaryData = dataURItoBlob(srcUrl);
+  const srcUrl = info.srcUrl;
+  const mimeType = srcUrl.substring(
+    srcUrl.indexOf(":") + 1,
+    srcUrl.indexOf(";")
+  );
+  const binaryData = dataURItoBlob(srcUrl);
   const url = "https://photoslibrary.googleapis.com/v1/uploads";
-  console.log(mimeType);
 
   chrome.identity.getAuthToken({ interactive: true }, function (token) {
-    // console.log(token);
     authorization = `Bearer ${token}`;
 
-    headers = {
+    const uploadHeaders = {
       Authorization: authorization,
       "Content-type": "application/octet-stream",
       "X-Goog-Upload-Content-Type": mimeType,
       "X-Goog-Upload-Protocol": "raw",
-      // Accept: "application/octet-stream",
     };
     fetch(url, {
       method: "POST",
-      headers: headers,
+      headers: uploadHeaders,
       body: binaryData,
     })
       .then((response) => response.text())
       .then((data) => {
         createURL =
           "https://photoslibrary.googleapis.com/v1/mediaItems:batchCreate";
-        // console.log(data);
-        body = {
+        const body = {
           newMediaItems: [
             {
               description: "item-description",
@@ -64,13 +53,13 @@ chrome.contextMenus.onClicked.addListener((info) => {
             },
           ],
         };
-        headers = {
+        const createHeaders = {
           "Content-Type": "application/json",
           Authorization: authorization,
         };
         fetch(createURL, {
           method: "POST",
-          headers: headers,
+          headers: createHeaders,
           body: JSON.stringify(body),
         })
           .then((response) => response.text())
